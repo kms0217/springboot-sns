2281a2e950d289d6092e8486f3fc0564cb883934
kms <kmslkh@naver.com>
2021-07-20 14:27:12 +0900

    [INIT] init.

---
+++ src/main/java/com/kms/mygram/MygramApplication.java
@@ -0,0 +1,13 @@
+package com.kms.mygram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MygramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MygramApplication.class, args);
+    }
+
+}
