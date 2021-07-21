e8aaae405afe13f9c71a8f7d3a12942cae4a9dc6
kms <kmslkh@naver.com>
2021-07-21 18:06:00 +0900

    [FIX] fix typo

---
+++ src/main/java/com/kms/mygram/config/SecurityConfig.java
@@ -0,0 +1,49 @@
+package com.kms.mygram.configure;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .authorizeRequests(request->{
+                    request
+                            .antMatchers("/login").permitAll()
+                            .antMatchers("/login-error").permitAll()
+                            .antMatchers("/signup").permitAll()
+                            .anyRequest().authenticated();
+                })
+                .formLogin(login-> {
+                    login
+                            .loginPage("/login")
+                            .loginProcessingUrl("/login")
+                            .defaultSuccessUrl("/", false)
+                            .failureUrl("/login-error");
+                });
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/css/**");
+        web.ignoring().antMatchers("/image/**");
+        web.ignoring().antMatchers("/js/**");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+}
