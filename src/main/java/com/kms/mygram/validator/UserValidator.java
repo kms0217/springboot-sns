8b4d34a1c85f1bc9ff9f927bb485df76f0c57d3b
kms <kmslkh@naver.com>
2021-07-21 15:27:35 +0900

    [UPDATE] username validation update

--- src/main/java/com/kms/mygram/validator/UserValidator.java
+++ src/main/java/com/kms/mygram/validator/UserValidator.java
@@ -8,6 +8,9 @@ import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 @Component
 public class UserValidator implements Validator {
 
@@ -29,5 +32,22 @@ public class UserValidator implements Validator {
             errors.rejectValue("emailOrPhoneNumber", "key", "이미 존재하는 핸드폰 번호 입니다.");
         if (authService.getUserByUserName(userRequestDto.getUsername()).isPresent())
             errors.rejectValue("username", "key", "이미 존재하는 사용자 이름 입니다.");
+        checkUserName(userRequestDto.getUsername(), errors);
+    }
+
+    public void checkUserName(String username, Errors errors) {
+        String allowPattern = "^[0-9a-zA-Zㄱ-ㅎㅏ-ㅣ가-힣_.]+$";
+        String exceptionPattern = "^[0-9]+$";
+        Pattern p = Pattern.compile(allowPattern);
+        Matcher matcher = p.matcher(username);
+        if (matcher.matches()){
+            p = Pattern.compile(exceptionPattern);
+            matcher = p.matcher(username);
+            if (matcher.matches()){
+                errors.rejectValue("username", "key", "사용자 이름에는 숫자만 포함될 수 없습니다.");
+            }
+        } else{
+            errors.rejectValue("username", "key", "사용자 이름에는 문자, 숫자, 밑줄 및 마침표만 사용할 수 있습니다.");
+       }
     }
 }
