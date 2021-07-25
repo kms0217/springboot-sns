c4438149df2b7d483f6662e609b2125a5eb9d96b
kms <kmslkh@naver.com>
2021-07-25 19:47:42 +0900

    [ADD] follow api 조회 부분 추가

--- src/main/java/com/kms/mygram/controller/api/FollowApiController.java
+++ src/main/java/com/kms/mygram/controller/api/FollowApiController.java
@@ -1,12 +1,16 @@
 package com.kms.mygram.controller.api;
 
 import com.kms.mygram.auth.Principal;
+import com.kms.mygram.domain.Follow;
 import com.kms.mygram.service.FollowService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api")
@@ -14,6 +18,31 @@ import org.springframework.web.bind.annotation.*;
 public class FollowApiController {
 
     private final FollowService followService;
+
+    @GetMapping("/follows")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public ResponseEntity<List<Follow>> allFollows() {
+        List<Follow> followList = followService.findAllFollows();
+        if(followList.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(followList, HttpStatus.OK);
+    }
+
+    @GetMapping("/follows/{userId}/follower")
+    public ResponseEntity<List<Follow>> getFollower(@PathVariable Long userId) {
+        List<Follow> followList = followService.findAllByFollower(userId);
+        if(followList.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(followList, HttpStatus.OK);
+    }
+
+    @GetMapping("/follows/{userId}/followee")
+    public ResponseEntity<List<Follow>> getFollowee(@PathVariable Long userId) {
+        List<Follow> followList = followService.findAllByFollowee(userId);
+        if(followList.isEmpty())
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        return new ResponseEntity<>(followList, HttpStatus.OK);
+    }
 
     @PostMapping("/follow/{userId}")
     public ResponseEntity Follow(@AuthenticationPrincipal Principal principal,
