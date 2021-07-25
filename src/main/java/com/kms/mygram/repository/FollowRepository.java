7870340edbd7c6b379f4c8cfeff3eac550a99b5f
kms <kmslkh@naver.com>
2021-07-25 19:45:16 +0900

    [ADD] follower, followee기준으로 조회 기능, Count 기능 추가,

--- src/main/java/com/kms/mygram/repository/FollowRepository.java
+++ src/main/java/com/kms/mygram/repository/FollowRepository.java
@@ -6,6 +6,8 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface FollowRepository extends JpaRepository<Follow, Long> {
 
     @Modifying
@@ -15,4 +17,19 @@ public interface FollowRepository extends JpaRepository<Follow, Long> {
     @Modifying
     @Query(value = "delete from follow where follower_id=:follower_id and followee_id=:followee_id", nativeQuery = true)
     int unfollow(@Param("follower_id") Long follower_id, @Param("followee_id") Long followee_id);
+
+    @Query(value = "select * from follow where followee_id=:followee_id", nativeQuery = true)
+    List<Follow> findAllByFollower(@Param("followee_id") Long userId);
+
+    @Query(value = "select * from follow where follower_id=:follower_id", nativeQuery = true)
+    List<Follow> findAllByFollowee(@Param("follower_id") Long userId);
+
+    @Query(value = "select count(*) from follow where followee_id=:followee_id", nativeQuery = true)
+    int followerCount(@Param("followee_id") Long userId);
+
+    @Query(value = "select count(*) from follow where follower_id=:follower_id", nativeQuery = true)
+    int followeeCount(@Param("follower_id") Long userId);
+
+    @Query(value = "select count(*) from follow where follower_id=:follower_id and followee_id=:followee_id", nativeQuery = true)
+    int checkFollowTarget(@Param("follower_id") Long userId, @Param("followee_id") Long targetUserId);
 }
--- src/main/java/com/kms/mygram/service/FollowService.java
+++ src/main/java/com/kms/mygram/service/FollowService.java
@@ -8,6 +8,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class FollowService {
@@ -15,21 +17,45 @@ public class FollowService {
     private final FollowRepository followRepository;
     private final UserService userService;
 
+    public List<Follow> findAllFollows() {
+        return followRepository.findAll();
+    }
+
     @Transactional
-    public void createFollow(Long followerId, Long followeeId){
-        try{
+    public void createFollow(Long followerId, Long followeeId) {
+        try {
             followRepository.follow(followerId, followeeId);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new ApiException("Follow 실패");
         }
     }
 
     @Transactional
-    public void deleteFollow(Long followerId, Long followeeId){
-        try{
+    public void deleteFollow(Long followerId, Long followeeId) {
+        try {
             followRepository.unfollow(followerId, followeeId);
-        } catch (Exception e){
+        } catch (Exception e) {
             throw new ApiException("Follow 상태가 아닙니다.");
         }
+    }
+
+    public int getFollowerNum(Long userId) {
+        return followRepository.followerCount(userId);
+    }
+
+    public int getFolloweeNum(Long userId) {
+        return followRepository.followeeCount(userId);
+    }
+
+    public boolean checkFollow(Long userId, Long targetUserId) {
+        return followRepository.checkFollowTarget(userId, targetUserId) > 0;
+    }
+
+    public List<Follow> findAllByFollower(Long userId) {
+        return followRepository.findAllByFollower(userId);
+    }
+
+    public List<Follow> findAllByFollowee(Long userId) {
+        return followRepository.findAllByFollowee(userId);
     }
 }
