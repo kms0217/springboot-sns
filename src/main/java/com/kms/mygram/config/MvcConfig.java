8cc8897087a323a1a0b466f7744c5ab17d1e3170
kms <kmslkh@naver.com>
2021-08-04 21:47:24 +0900

    [UPDATE] MvcConfig profile마다  ResourceLocation 다르게 설정

--- src/main/java/com/kms/mygram/config/MvcConfig.java
+++ src/main/java/com/kms/mygram/config/MvcConfig.java
@@ -2,22 +2,44 @@ package com.kms.mygram.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.resource.PathResourceResolver;
 
 @Configuration
-public class MvcConfig implements WebMvcConfigurer {
+public class MvcConfig{
 
     @Value("${upload.path}")
     private String rootPath;
 
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry
-                .addResourceHandler("/file/**")
-                .addResourceLocations("file:///" + rootPath)
-                .resourceChain(true)
-                .addResolver(new PathResourceResolver());
+    @Profile("local")
+    @Configuration
+    public class LocalMvcConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry
+                    .addResourceHandler("/file/**")
+                    .addResourceLocations("file:///" + rootPath)
+                    .resourceChain(true)
+                    .addResolver(new PathResourceResolver());
+        }
+
+    }
+
+    @Profile("prod")
+    @Configuration
+    public class ProdMvcConfig implements WebMvcConfigurer {
+
+        @Override
+        public void addResourceHandlers(ResourceHandlerRegistry registry) {
+            registry
+                    .addResourceHandler("/file/**")
+                    .addResourceLocations("file:/" + rootPath)
+                    .resourceChain(true)
+                    .addResolver(new PathResourceResolver());
+        }
+
     }
 }
